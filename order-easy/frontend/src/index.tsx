940320cc71c27fd73271ea5b23d6ca29d3c9ac48
jdanprad0 <jdanprad0@gmail.com>
2023-06-29 13:33:15 -0300

    instrucao de instalacao

--- order-easy/frontend/src/index.tsx
+++ order-easy/frontend/src/index.tsx
@@ -1,5 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {App} from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Não foi possível encontrar o elemento raiz 'root'.");
+}
